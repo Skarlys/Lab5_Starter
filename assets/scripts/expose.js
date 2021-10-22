a7a9a4b0f8d72ba37ab5c9723b36d0e4543de153
Danica <danica.xiong@yahoo.com>
2021-10-21 21:35:32 -0700

    finished expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -8,29 +8,27 @@ function init() {
   const con = new JSConfetti();
   
   var image = document.getElementsByTagName('img')[0];
-  var horn = document.getElementsById('horn-select');
+  var horn = document.getElementById('horn-select');
   var range = document.getElementById('volume');
-  
-  var playSound = document.getElementByTagName('button')[0];
-  var sound = document.getElementByTagName('audio')[0];
+  var playSound = document.getElementsByTagName('button')[0];
+  var sound = document.getElementsByTagName('audio')[0];
 
-  volume.addEventListener('change', (event) => {
+  
+  range.addEventListener('input', (event) => {
     if (range.value == 0){
       document.getElementsByTagName('img')[1].src = "./assets/icons/volume-level-0.svg";
-      sound.volume = 0;
     }
     else if (range.value > 0 && range.value<33){
       document.getElementsByTagName('img')[1].src = "./assets/icons/volume-level-1.svg";
-      sound.volume = 0;
     }
     else if (range.value >= 33 && range.value<67){
       document.getElementsByTagName('img')[1].src = "./assets/icons/volume-level-2.svg";
-      sound.volume = 0;
     }
     else{
       document.getElementsByTagName('img')[1].src = "./assets/icons/volume-level-3.svg";
-      sound.volume = 0;
     }
+    sound.volume = volume.value * 0.01;
+
   });
 
   playSound.addEventListener('click', (event) => {
@@ -45,17 +43,16 @@ function init() {
   if (horn.value == 'air-horn'){
     image.src = "./assets/images/air-horn.svg";
     sound.src = "./assets/audio/air-horn.mp3";
-  } else if (selectedHorn.value == 'car-horn'){
+  } else if (horn.value == 'car-horn'){
     image.src = "./assets/images/car-horn.svg";
     sound.src = "./assets/audio/car-horn.mp3";
   }
-  else {
+  else if (horn.value == 'party-horn') {
     image.src = "./assets/images/party-horn.svg";
     sound.src = "./assets/audio/party-horn.mp3";
   }
 
 
   });
-
 
 }
