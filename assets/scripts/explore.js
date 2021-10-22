f681880dc1e8044b1c38da342993b10f5a668d12
Danica <danica.xiong@yahoo.com>
2021-10-22 12:27:47 -0700

    i think i finished explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,26 +2,58 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
-function init() {
+  let synth = window.speechSynthesis;
+  let voiceSelect = document.getElementById('voice-select');
+  let select = document.querySelector('select');
+  //let selectedOption;
   
-  var synth = window.speechSynthesis;
-  var voices = synth.getVoices();
-  var voiceSelect = document.getElementById('voice-select');
-  var select = document.querySelector('select');
-  for(var i = 0; i < voices.length ; i++) {
 
-    var option = document.createElement('option');
-    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
-
-    if(voices[i].default) {
-      option.textContent += ' -- DEFAULT';
-    }
-
-    voiceSelect.appendChild(option);
+function init() {
+  if (synth.onvoiceschanged !== undefined){
+    populateVoiceList();
   }
 
-  let voice_chosen;
-  select.addEventListener("change", (event) =>{
+  let play = document.getElementsByTagName('button')[0];
+  let text = document.getElementById('text-to-speak');
 
-  })
+
+  play.addEventListener('click', (event) => {
+
+  var utterThis = new SpeechSynthesisUtterance(text.value);
+  let face = document.getElementsByTagName('img')[0];
+  face.src = './assets/images/smiling-open.png';
+  utterThis.onend = function() {face.src = './assets/images/smiling.png'};
+
+
+  // from: https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis
+  let voices = synth.getVoices();
+  var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+  for(var i = 0; i < voices.length ; i++) {
+    if(voices[i].name === selectedOption) {
+      utterThis.voice = voices[i];
+    }
+  }
+  synth.speak(utterThis);
+
+  });
+
 }
+
+// from: https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis
+function populateVoiceList(){
+
+  synth.addEventListener('voiceschanged', (event)=>{
+    let voices = synth.getVoices();
+
+  for(var i = 0; i < voices.length ; i++) {
+    var option = document.createElement('option');
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+    
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+});
+
+}
+
